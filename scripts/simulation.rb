36d4499492bcdd1016fb89f9cb3339f17f8d0613
Raul Dominguez <raul.dominguez@dfki.de>
2016-05-31 19:17:41 +0200

    The test script for the simulation is updated. - New tasks network with skid odometry, simple controller and no bodytask. TODO: The odometry does not provide any output value TODO: The velodyne task fails to start

--- scripts/simulation.rb
+++ scripts/simulation.rb
@@ -7,12 +7,13 @@ Orocos.initialize
 Orocos.run 'simulation_asguard' do
 
     mars = TaskContext.get 'mars'
-    velodyne = TaskContext.get 'velodyne'
     mars_actuators = TaskContext.get 'mars_actuators'
+    sysmon = TaskContext.get 'sysmon'
     joint_dispatcher = TaskContext.get 'joint_dispatcher'
+    simple_controller = TaskContext.get 'simple_controller'
     xsens = TaskContext.get 'xsens'
-    controller = TaskContext.get 'simple_controller'
-    body_state = TaskContext.get 'body_state'
+    odometry = TaskContext.get 'odometry'
+    #velodyne = TaskContext.get 'velodyne'
     
 #    mars.controller_port = 1600
 #    mars.enable_gui = 1
@@ -27,57 +28,46 @@ Orocos.run 'simulation_asguard' do
 #
 #    mars.raw_options = raw_options
     
+    # I am not sure about the configs, e.g. maybe the default have to be applied too
     mars.apply_conf_file("mars::Task.yml", ["default"])
     mars.configure
     mars.start
-
-    
-    sleep 10
-    
-    # I am not sure about the configs, e.g. maybe the default have to be applied too
-    
-    joint_dispatcher.apply_conf_file("joint_dispatcher::Task.yml", ["default"])
-    joint_dispatcher.configure
-    
+    #sleep 10
     mars_actuators.apply_conf_file("mars::Joints.yml", ["base"]) 
     mars_actuators.configure
-    
-    velodyne.apply_conf_file("mars::RotatingLaserRangeFinder.yml", ["default"])
-    velodyne.configure
-    
+    sysmon.apply_conf_file("mars::Joints.yml", ["sysmon"])
+    sysmon.configure
+    joint_dispatcher.apply_conf_file("joint_dispatcher::Task.yml", ["default"])
+    joint_dispatcher.configure
+    simple_controller.apply_conf_file("skid4_control::SimpleController.yml", ["default"])
+    simple_controller.configure
     xsens.apply_conf_file("mars::IMU.yml", ["default"])
     xsens.configure
-    
-    body_state.apply_conf_file("asguard::BodyTask.yml", ["default"])
-    body_state.configure
-
-    controller.apply_conf_file("skid4_control::SimpleController.yml", ["default"])
-    controller.configure
-    
+    odometry.apply_conf_file("odometry::Skid.yml", ["asguard"])
+    odometry.configure
+    #velodyne.apply_conf_file("mars::RotatingLaserRangeFinder.yml", ["default"])
+    #velodyne.configure
     # Connections
     joint_dispatcher.command_out.connect_to(mars_actuators.command, :type=>:buffer, :size=>100)
     mars_actuators.status_samples.connect_to(joint_dispatcher.status_samples_in, :type=>:buffer, :size=>100)
-    
-    joint_dispatcher.status_samples.connect_to(body_state.actuator_samples, :type => :buffer, :size => 100)
-    #body_state.contact_samples.connect_to(odometry.contact_samples, :type=>:buffer, :size=>100)
-
-    controller.command.connect_to(joint_dispatcher.command, :type=>:buffer, :size=>100)
-    joint_dispatcher.status_samples.connect_to(controller.status_samples, :type=>:buffer, :size=>100)
-
-    body_state.start 
-    xsens.start
-    joint_dispatcher.start
+    sysmon.status_samples.connect_to(joint_dispatcher.body_joint_in, :type=>:buffer, :size=>100)
+    simple_controller.command.connect_to(joint_dispatcher.command, :type=>:buffer, :size=>100)
+    joint_dispatcher.status_samples.connect_to(simple_controller.status_samples, :type=>:buffer, :size=>100)
+    joint_dispatcher.status_samples.connect_to(odometry.actuator_samples, :type=>:buffer, :size=>100)
+    xsens.orientation_samples.connect_to(odometry.dynamic_transformations, :type=>:buffer, :size=>100)
     mars_actuators.start
-    velodyne.start
-    controller.start
-    
+    sysmon.start
+    joint_dispatcher.start
+    simple_controller.start
+    xsens.start
+    odometry.start
+    #velodyne.start
     #
 #    STDOUT.puts "Restartign mars"
 #    mars.stop
 #    mars.cleanup
 #    mars.configure
 #    mars.start
-
     Readline::readline("Press ENTER to quit") do
     end
 
