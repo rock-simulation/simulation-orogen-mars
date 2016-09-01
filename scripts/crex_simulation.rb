60bc49e6af3b1c51ccea203c3815e64e61165aae
Raul Dominguez <raul.dominguez@dfki.de>
2016-09-01 16:45:24 +0200

    Some test for crex model and for crex with velodyne.

---
+++ scripts/crex_simulation.rb
@@ -0,0 +1,26 @@
+require 'orocos'
+require 'readline'
+include Orocos
+
+Orocos.initialize
+
+Orocos.run 'crex_simulation', :output=>nil  do
+
+    mars = TaskContext.get 'mars'
+    xsens = TaskContext.get 'xsens'
+    velodyne = TaskContext.get 'velodyne'
+    mars.apply_conf_file("mars::Task.yml", ["default", "crex_in_dlr_scene"])
+    mars.configure
+    mars.start
+    xsens.apply_conf_file("mars::IMU.yml", ["default"])
+    xsens.configure
+    velodyne.apply_conf_file("mars::RotatingLaserRangeFinder.yml", ["default"])
+    velodyne.configure
+    # Connections
+    xsens.start
+    velodyne.start
+  
+    Readline::readline("Press ENTER to quit") do
+    end
+
+end 
