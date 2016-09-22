a6c4f099a63126050d9f98f3275b396aaeaf3257
Anna Born <anna.born@dfki.de>
2016-09-22 15:56:02 +0200

    add joint6dof sensor into crex_simulation script

--- scripts/crex_simulation.rb
+++ scripts/crex_simulation.rb
@@ -9,16 +9,25 @@ Orocos.run 'crex_simulation', :output=>nil  do
     mars = TaskContext.get 'mars'
     xsens = TaskContext.get 'xsens'
     velodyne = TaskContext.get 'velodyne'
+    ft_sensors = TaskContext.get 'ft_sensors'
+
     mars.apply_conf_file("mars::Task.yml", ["default", "crex_in_dlr_scene"])
     mars.configure
     mars.start
+
     xsens.apply_conf_file("mars::IMU.yml", ["default"])
     xsens.configure
+
     velodyne.apply_conf_file("mars::RotatingLaserRangeFinder.yml", ["default"])
     velodyne.configure
+
+    ft_sensors.apply_conf_file("mars::ForceTorque6DOF.yml", ["default"])
+    ft_sensors.configure
+
     # Connections
     xsens.start
     velodyne.start
+    ft_sensors.start
   
     Readline::readline("Press ENTER to quit") do
     end
