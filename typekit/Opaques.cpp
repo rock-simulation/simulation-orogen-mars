d06789032e703a350e80101c6cccafc824f759fd
Steffen Planthaber <steffen.planthaber@dfki.de>
2022-08-23 14:27:57 +0200

    remove doc

--- typekit/Opaques.cpp
+++ typekit/Opaques.cpp
@@ -13,8 +13,6 @@
 
 void orogen_typekits::toIntermediate(::wrappers::Matrix< double, 3, 1 >& intermediate, ::mars::utils::Vector const& real_type)
 {
-    //re-use Eigen opaque conversion from base/types
-    // toIntermediate(intermediate, real_type);
     intermediate.data[0] = real_type.x();
     intermediate.data[1] = real_type.y();
     intermediate.data[2] = real_type.z();
@@ -22,8 +20,6 @@ void orogen_typekits::toIntermediate(::wrappers::Matrix< double, 3, 1 >& interme
 
 void orogen_typekits::fromIntermediate(::mars::utils::Vector& real_type, ::wrappers::Matrix< double, 3, 1 > const& intermediate)
 {
-    //re-use Eigen opaque conversion from base/types
-    // fromIntermediate(real_type, intermediate);
     real_type.x() = intermediate.data[0];
     real_type.y() = intermediate.data[1];
     real_type.z() = intermediate.data[2];
